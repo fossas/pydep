09558dae7c68cb308dbe53d459d18eb6b479494b
Abraham Elmahrek <abe@tldrlegal.com>
2016-11-17 13:41:06 -0800

    Add logging configuration

--- pydep-run.py
+++ pydep-run.py
@@ -11,6 +11,31 @@ from os import path
 import subprocess
 import tempfile
 import shutil
+import logging
+
+logging_config = dict(
+  version = 1,
+  formatters = {
+    "f": {
+      "format":
+      "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+    }
+  },
+  handlers = {
+    "h": {
+      "class": "logging.StreamHandler",
+      "formatter": "f",
+      "level": logging.DEBUG,
+      "stream": "ext://sys.stderr"
+    }
+  },
+  root = {
+    "handlers": ["h"],
+    "level": logging.DEBUG,
+  },
+)
+
+logging.config.dictConfig(logging_config)
 
 
 def main():
